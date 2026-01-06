d60712ec157d12c50d24bdf77bdda599e163e2b2
mestrereginaldo-site <reginaldo.oliveira@catolicasc.edu.br>
2026-01-06 17:47:05 -0300

    Create next.config.ts

---
+++ next.config.ts
@@ -0,0 +1,8 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* Configurações adicionais podem ser adicionadas aqui */
+  reactStrictMode: true,
+};
+
+export default nextConfig;
